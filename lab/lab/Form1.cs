7dc7016124e6ffa4d93a9a1395a743514adedb75
Skkorty <144678299+Skkorty@users.noreply.github.com>
2023-10-23 17:41:56 +0800

    Add files via upload

---
+++ lab/lab/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Thread ThreadA, ThreadB, ThreadC, ThreadD;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ThreadStart start = new ThreadStart(MyThreadClass.Thread1);
+            ThreadStart start2 = new ThreadStart(MyThreadClass.Thread2);
+            ThreadA = new Thread(start);
+            ThreadB = new Thread(start2);
+            ThreadC = new Thread(start);
+            ThreadD = new Thread(start2);
+            ThreadA.Name = "Thread A Process";
+            ThreadB.Name = "Thread B Process";
+            ThreadC.Name = "Thread C Process";
+            ThreadD.Name = "Thread D Process";
+            ThreadA.Priority = ThreadPriority.Highest;
+            ThreadB.Priority = ThreadPriority.Normal;
+            ThreadC.Priority = ThreadPriority.AboveNormal;
+            ThreadD.Priority = ThreadPriority.BelowNormal;
+            Console.WriteLine(label1.Text.ToString());
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadC.Start();
+            ThreadD.Start();
+            ThreadA.Join();
+            ThreadB.Join();
+            ThreadC.Join();
+            ThreadD.Join();
+            label1.Text = "-End of thread-";
+            Console.WriteLine(label1.Text.ToString());
+        }
+    }
+}
---
+++ lab/lab/MyThreadClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            
+            for (int LoopCount = 0; LoopCount <= 2; LoopCount++) 
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of thread: " + thread.Name + " = " + LoopCount);
+                Thread.Sleep(500);
+            }
+            
+            
+        }
+        public static void Thread2() 
+        {
+            for (int LoopCount = 0; LoopCount <= 5; LoopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of thread: " + thread.Name + " = " + LoopCount);
+                Thread.Sleep(2000);
+            }
+        }
+
+    }
+    
+}
